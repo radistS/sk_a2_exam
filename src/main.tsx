d81c9c2ed97121f50b9a4818e07a8fc777222a16
Alex Stepurko <Stepurko.alexandr@gmail.com>
2026-08-15 22:40:42 +0200

    feat: switch preview to five complete A2 topics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles.css';
+import App5 from './App5';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <App5 />
   </React.StrictMode>
 );
